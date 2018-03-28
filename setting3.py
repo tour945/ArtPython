9dbdba178d061e1b5971a9c0a21f4c500aedede8
tour945 <37860460+tour945@users.noreply.github.com>
2018-03-28 17:24:08 +0800

    Add files via upload

---
+++ setting3.py
@@ -0,0 +1,16 @@
+﻿# setting.py
+# 2018-03-28
+# python3
+# ubuntu 64bit 16.04
+#
+import matplotlib.pyplot as plot
+import ArtPython.TransModule3
+#
+def graphic_area(width, height):
+    global tup7
+    tup7 = TransModule3.set_graphic_area(width, height)
+        
+def fig_ax():
+    global ax, fig
+    fig = plot.figure(figsize=(tup7[0], tup7[1]))
+    ax = fig.add_subplot(1,1,1)
